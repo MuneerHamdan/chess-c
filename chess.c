30bdc403f93f9e4fe4b758e832da3388c178ed3c
Muneer Hamdan <muneerhamdan01@gmail.com>
2025-05-05 13:36:03 -0400

    movement

--- chess.c
+++ chess.c
@@ -31,7 +31,6 @@ Board* initboard() {
 	Piece* wp6 = initpiece(board, 'w', 'p', 6, 6);
 	Piece* wp7 = initpiece(board, 'w', 'p', 6, 7);
 
-
 	Piece* bp0 = initpiece(board, 'b', 'p', 1, 0);
 	Piece* bp1 = initpiece(board, 'b', 'p', 1, 1);
 	Piece* bp2 = initpiece(board, 'b', 'p', 1, 2);
@@ -40,7 +39,33 @@ Board* initboard() {
 	Piece* bp5 = initpiece(board, 'b', 'p', 1, 5);
 	Piece* bp6 = initpiece(board, 'b', 'p', 1, 6);
 	Piece* bp7 = initpiece(board, 'b', 'p', 1, 7);
-	
+
+
+	Piece* wr1 = initpiece(board, 'w', 'r', 7, 0);
+	Piece* wr2 = initpiece(board, 'w', 'r', 7, 7);
+
+	Piece* br1 = initpiece(board, 'b', 'r', 0, 0);
+	Piece* br2 = initpiece(board, 'b', 'r', 0, 7);
+
+
+	Piece* wb1 = initpiece(board, 'w', 'b', 7, 1);
+	Piece* wb2 = initpiece(board, 'w', 'b', 7, 6);
+
+	Piece* bb1 = initpiece(board, 'b', 'b', 0, 1);
+	Piece* bb2 = initpiece(board, 'b', 'b', 0, 6);
+
+
+	Piece* wn1 = initpiece(board, 'w', 'n', 7, 2);
+	Piece* wn2 = initpiece(board, 'w', 'n', 7, 5);
+
+	Piece* bn1 = initpiece(board, 'b', 'n', 0, 2);
+	Piece* bn2 = initpiece(board, 'b', 'n', 0, 5);
+
+	Piece* wK = initpiece(board, 'w', 'K', 7, 3);
+	Piece* wQ = initpiece(board, 'w', 'Q', 7, 4);
+
+	Piece* bK = initpiece(board, 'b', 'K', 0, 3);
+	Piece* bQ = initpiece(board, 'b', 'Q', 0, 4);
 
 	return board;
 }
@@ -61,12 +86,14 @@ Piece* initpiece(Board* board, char type, char color, int posx, int posy) {
 
 void drawboard(Board* board) {
 
-	int ch;
+	int ch, x, y;
+	ch = x = y = 0;
 
 	initscr();
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
+//	getmaxyx(stdscr, y, x);
 
 	printw("welcome to chess! press ESC to quit, any other to continue...");
 
@@ -81,18 +108,40 @@ void drawboard(Board* board) {
 					printw("%c%c ", board->tiles[i][j].piece->type, board->tiles[i][j].piece->color);
 				}
 				else {
-					printw(" %c ", board->tiles[i][j].empty);
+					printw("%c%c ", board->tiles[i][j].empty, board->tiles[i][j].empty);
 				}
 
 			}
-		printw("\n");
+			printw("\n");
 
-	}
+		}
 
+		printw("%d, %d", y, x);
+
+
+		switch (ch) {
+			case KEY_UP:
+				y--;
+				break;
+			case KEY_DOWN:
+				y++;
+				break;
+			case KEY_LEFT:
+				x--;
+				break;
+			case KEY_RIGHT:
+				x++;
+				break;
+			default:
+				break;
+		}
+
+		move(y, x);
 
 		refresh();
 
 	}
+
 
 	refresh();
 	endwin();
